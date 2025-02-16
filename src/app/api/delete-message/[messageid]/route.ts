735c104cd6b70e99c6cd3d5f4981ccd276061b05
shubham72-73 <shubham2003pd@gmail.com>
2025-02-16 16:43:23 +0530

    Updated messageId

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -1,3 +1,54 @@
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "../../auth/[...nextauth]/options";
+// import { User } from "next-auth";
+// import dbConnect from "@/lib/dbConnect";
+// import UserModel from "@/model/user";
+
+// export async function DELETE(request: Request, {params}: {params: {messageid: string}}){
+//     const messageId = params.messageid
+//     await dbConnect();
+//     const session = await getServerSession(authOptions);
+//     const user:User = session?.user as User
+
+//     if(!session || !session.user){
+//         return Response.json(
+//             {
+//                 success: false,
+//                 message: 'Not Authenticated'
+//             },{status: 401}
+//         )
+//     }
+//     try {
+//         const updateResult = await UserModel.updateOne(
+//             {_id: user._id},
+//             {$pull: {messages: {_id: messageId}}}
+//         )
+//         if(updateResult.modifiedCount == 0){
+//             return Response.json(
+//                 {
+//                     success: false,
+//                     message: 'Message not found or already deleted'
+//                 },{status: 404}
+//             )
+//         }
+//         return Response.json(
+//             {
+//                 success: true,
+//                 message: 'Message deleted'
+//             },
+//             {status: 200}
+//         )
+//     } catch (error) {
+//         console.log('Error in deleting message route', error)
+//         return Response.json(
+//             {
+//                 success: false,
+//                 message: 'Error deleting message'
+//             },{status: 500}
+//         )
+//     }
+// }
+
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
 import { User } from "next-auth";
@@ -8,7 +59,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function DELETE(request: NextRequest) {
     // Extract the messageid from the URL
     const url = new URL(request.url);
-    const messageid = url.pathname.split("/")[4]; // Assuming the dynamic segment is the 4th part of the URL
+    const messageid = request.nextUrl.pathname.split("/").pop();
 
     await dbConnect();
     const session = await getServerSession(authOptions);
