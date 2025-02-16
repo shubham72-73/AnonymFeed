7a872f40da6ac507fb1169aeb784404b1f1b095e
shubham72-73 <shubham2003pd@gmail.com>
2025-02-16 16:04:35 +0530

    Bug fixes

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -1,32 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
 import { User } from "next-auth";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/user";
 
-export async function DELETE(request: Request, context: { params: { messageid: string } }) {
-    const { messageid } = context.params;
+export async function DELETE(request: NextRequest, { params }: { params: { messageid: string } }) {
+    const messageId = params.messageid;
     await dbConnect();
     const session = await getServerSession(authOptions);
     const user: User = session?.user as User;
 
     if (!session || !session.user) {
-        return new Response(JSON.stringify({ success: false, message: "Not Authenticated" }), { status: 401 });
+        return NextResponse.json({ success: false, message: "Not Authenticated" }, { status: 401 });
     }
 
     try {
         const updateResult = await UserModel.updateOne(
             { _id: user._id },
-            { $pull: { messages: { _id: messageid } } }
+            { $pull: { messages: { _id: messageId } } }
         );
 
         if (updateResult.modifiedCount === 0) {
-            return new Response(JSON.stringify({ success: false, message: "Message not found or already deleted" }), { status: 404 });
+            return NextResponse.json({ success: false, message: "Message not found or already deleted" }, { status: 404 });
         }
 
-        return new Response(JSON.stringify({ success: true, message: "Message deleted" }), { status: 200 });
+        return NextResponse.json({ success: true, message: "Message deleted" }, { status: 200 });
     } catch (error) {
         console.error("Error in deleting message route", error);
-        return new Response(JSON.stringify({ success: false, message: "Error deleting message" }), { status: 500 });
+        return NextResponse.json({ success: false, message: "Error deleting message" }, { status: 500 });
     }
 }
