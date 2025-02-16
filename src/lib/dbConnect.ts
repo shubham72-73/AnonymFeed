76ebf1541d8f2d8f57ec3c7107de09ab92099f43
shubham72-73 <shubham2003pd@gmail.com>
2025-02-16 12:27:27 +0530

    Updated dbConnect

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -7,17 +7,23 @@ type ConnectionObject = {
 const connection: ConnectionObject = {}
 
 async function dbConnect(): Promise<void> {
-    if(connection.isConnected){
+    if (connection.isConnected) {
         console.log("Already connected to database");
-        return
+        return;
     }
-    try {
-        const db = await mongoose.connect(process.env.MONGODB_URI, {})
 
+    // Ensure that the MONGODB_URI is defined
+    const mongoUri = process.env.MONGODB_URI;
+    if (!mongoUri) {
+        console.error("MONGODB_URI environment variable is not defined");
+        process.exit(1);
+    }
+
+    try {
+        const db = await mongoose.connect(mongoUri, {});
         connection.isConnected = db.connections[0].readyState;
         console.log("DB Connected Successfully");
-        console.log(process.env.MONGODB_URI)
-        
+        console.log(mongoUri);
     } catch (error) {
         console.log("Database Connection Failed", error);
         process.exit(1);
