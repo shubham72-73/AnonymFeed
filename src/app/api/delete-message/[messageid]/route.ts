e31550c99553937892e1800f23fc16aeaa9a89a6
shubham72-73 <shubham2003pd@gmail.com>
2025-02-16 11:18:53 +0530

    Fixed ESlint Errors

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -4,47 +4,59 @@ import { User } from "next-auth";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/user";
 
-export async function DELETE(request: Request, {params}: {params: {messageid: string}}){
-    const messageId = params.messageid
-    await dbConnect();
-    const session = await getServerSession(authOptions);
-    const user:User = session?.user as User
+// Define a type for the route parameters
+interface Params {
+  messageid: string;
+}
 
-    if(!session || !session.user){
-        return Response.json(
-            {
-                success: false,
-                message: 'Not Authenticated'
-            },{status: 401}
-        )
+export async function DELETE(request: Request, { params }: { params: Params }) {
+  const messageId = params.messageid;
+
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const updateResult = await UserModel.updateOne(
+      { _id: user._id },
+      { $pull: { messages: { _id: messageId } } }
+    );
+
+    if (updateResult.modifiedCount === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "Message not found or already deleted",
+        },
+        { status: 404 }
+      );
     }
-    try {
-        const updateResult = await UserModel.updateOne(
-            {_id: user._id},
-            {$pull: {messages: {_id: messageId}}}
-        )
-        if(updateResult.modifiedCount == 0){
-            return Response.json(
-                {
-                    success: false,
-                    message: 'Message not found or already deleted'
-                },{status: 404}
-            )
-        }
-        return Response.json(
-            {
-                success: true,
-                message: 'Message deleted'
-            },
-            {status: 200}
-        )
-    } catch (error) {
-        console.log('Error in deleting message route', error)
-        return Response.json(
-            {
-                success: false,
-                message: 'Error deleting message'
-            },{status: 500}
-        )
-    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message deleted",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error in deleting message route", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error deleting message",
+      },
+      { status: 500 }
+    );
+  }
 }
